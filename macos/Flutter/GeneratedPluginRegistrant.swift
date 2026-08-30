8616ebfa4f5194dbe414cd5509976e18f4562696
Jonathan-Juarez <jonhyvillatoro4@gmail.com>
2026-08-30 00:42:27 -0600

    build(deps): actualizar dependencias y configurar permisos de red y firma
    - Añadir dependencia multicast_dns y remover mobile_scanner.
    - Configurar permisos de multicast Wi-Fi en Android y NSLocalNetworkUsageDescription en iOS.
    - Configurar signingConfigs para release build y App Bundle con keystore en Gradle.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,7 +10,6 @@ import firebase_core
 import firebase_messaging
 import flutter_local_notifications
 import flutter_secure_storage_darwin
-import mobile_scanner
 import package_info_plus
 import printing
 import share_plus
@@ -23,7 +22,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
-  MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
